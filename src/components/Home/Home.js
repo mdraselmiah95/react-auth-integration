4dfa64b7475809867436e79b89347f2201221548
rasel <russelroy15@gmail.com>
2021-10-10 03:50:49 +0600

    WIP on React auth integration

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import useAuth from "../../hooks/useAuth";
 import "./Home.css";
 const Home = () => {
+  const { user } = useAuth();
   return (
     <div>
       <h2>This is Home</h2>
+      <h5 className="text-primary">User: {user.displayName}</h5>
     </div>
   );
 };
