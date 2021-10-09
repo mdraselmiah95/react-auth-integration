ac95a90b20b0e8160308c7fba35676c28711a3b3
rasel <russelroy15@gmail.com>
2021-10-09 18:25:13 +0600

    WIP on React auth integration project

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 const Login = () => {
   return (
-    <div>
-      <h2>This is login</h2>
+    <div className="my-3">
+      <h2 className="my-3">Please login</h2>
+      <button className="btn btn-primary">Google Sign In</button>
+      <Link
+        to="/register"
+        className="ms-2 text-decoration-none btn btn-outline-info"
+      >
+        New User
+      </Link>
     </div>
   );
 };
