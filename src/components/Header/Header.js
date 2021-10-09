d17c41e3c01a83de8c3c26fc178203f0af88cf9d
rasel <russelroy15@gmail.com>
2021-10-09 16:29:55 +0600

    WIP on React auth integration project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
-    <div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <NavbarBrand to="/home">BEauTy</NavbarBrand>
+          <Nav className="mx-auto">
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+/* 
+
+   <div>
       <Link to="/home">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
     </div>
-  );
-};
 
+
+
+*/
 export default Header;
