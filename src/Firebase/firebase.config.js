8c3c748a2ad44464c4b382478dfab9155d5091d4
rasel <russelroy15@gmail.com>
2021-10-09 16:09:14 +0600

    WIP on React auth integration project

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDfgNnPUJL8sOnnxCxlp5ieUmbx9ezln_s",
+  authDomain: "simple-firebase-authenti-73fb4.firebaseapp.com",
+  projectId: "simple-firebase-authenti-73fb4",
+  storageBucket: "simple-firebase-authenti-73fb4.appspot.com",
+  messagingSenderId: "128135917782",
+  appId: "1:128135917782:web:cae4ea2408f8ee550a5977",
+};
+export default firebaseConfig;
