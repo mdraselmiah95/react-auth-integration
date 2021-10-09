8e587d2f85eed32ef4973c1dca6b6a6d6c210932
rasel <russelroy15@gmail.com>
2021-10-09 16:02:42 +0600

    WIP on React auth integration project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Header from "../Header/Header";
 import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <Header></Header>
+      <h2>This is Home</h2>
     </div>
   );
 };
