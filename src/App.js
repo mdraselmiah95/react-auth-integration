45a213377ddedf57c957932b17ab4640b781f771
rasel <russelroy15@gmail.com>
2021-10-09 15:53:39 +0600

    WIP on React auth integration project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-primary">This is App Js </h2>
+      <Home></Home>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "../Header/Header";
 import "./Home.css";
 const Home = () => {
   return (
     <div>
-      <h2>This is Home.</h2>
+      <Header></Header>
     </div>
   );
 };
